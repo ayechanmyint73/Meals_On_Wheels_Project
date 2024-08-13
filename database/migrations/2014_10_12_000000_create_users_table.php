e8235e0a0ebc0e603a39debb7e90d96c41ac781e
John Edward Oblepias <johnedwardoblepias11@gmail.com>
2024-08-13 09:56:02 +0800

    Fixed the migration files so DBAL is not required

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -21,7 +21,7 @@ return new class extends Migration
             $table->string('phone');
             $table->string('address');
             $table->string('geolocation');
-            $table->string('role');
+            $table->string('role')->default('member');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
